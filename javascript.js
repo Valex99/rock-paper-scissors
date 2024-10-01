a6fdafc5e1abcd884e8b0b0db7d0b67517f1fcb4
valex <mval.fx@gmail.com>
2024-10-01 22:59:32 +0200

    Finish rock-paper-scisors game with score tracking

--- javascript.js
+++ javascript.js
@@ -2,89 +2,97 @@ let computerScore = 0;
 let humanScore = 0;
 
 
+// Function returns random computer choice for rock/paper/scissors
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
-/*    
-    // Output Rock/Paper/Scissors based on random number
-    if (computerChoice === 1) {
-        //console.log("Rock");
-    }
-    else if (computerChoice === 2) {
-        console.log("Paper");
-    }
-    else {
-        console.log("Scissors");
-    }
-*/
     // Return value of a function
     console.log(`Computer choice is: ${computerChoice}`);
     return computerChoice;
 }
 
 
+// Function checks for valid human input and returns human choice
 function getHumanChoice() {
-    // Get input from the user and turn it into integer
     let humanChoice = parseInt(prompt("Rock = 1, Paper = 2, Scissors = 3"));
 
     // Check for valid input
-    // Check if user input is a number between 1 and 3 - else call getHumanChoice function again
     if (humanChoice < 1 || humanChoice > 3 || (isNaN(humanChoice))) {
         alert("Wrong input");
         getHumanChoice();
     }
 
-
     console.log(`Human choice is: ${humanChoice}`);
-    console.log(typeof humanChoice)
     return humanChoice;
 }
 
-// Both functions above work
-// They both return int value
+
+// Function that plays entire game (5 rounds of playRound function)
+function playGame() {
+
+    // Function plays a single round of a game
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log("Tie!");
+        }
+        else if (humanChoice === 1) {
+            if (computerChoice === 2) {
+                console.log("Computer wins! Paper beats rock!");
+                computerScore += 1;
+            }
+            else {
+                console.log("Human wins! Rock beats scissors");
+                humanScore += 1;
+            }
+        }
+        else if (humanChoice === 2) {
+            if (computerChoice === 1) {
+                console.log("Human wins! Paper beats rock!");
+                humanScore += 1;
+            }
+            else {
+                console.log("Computer wins! Scissors beat paper!");
+                computerScore += 1; 
+            }
+        }
+        else {
+            if (computerChoice === 1) {
+                console.log("Computer wins! Rock beats scissors!");
+                computerScore += 1; 
+            }
+            else {
+                console.log("Human wins! Scissors beat paper!");
+                humanScore += 1; 
+            }
+        }
+    }
+    
+    // For loop that calls the playRound function 5 times
+    for (let i = 0; i < 5; i++) {
+        
+        // Define two new variables (const) and save result of human and computer choices in them
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        // Call playRound function and pass those two variables as arguments
+        playRound(humanSelection, computerSelection);
+    }
 
 
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        alert("Tie!");
+    // Determine who the winner is
+    if (computerScore > humanScore) {
+        console.log(`Computer wins with score ${computerScore} : ${humanScore}`);
     }
-    else if (humanChoice === 1 && computerChoice === 2) {
-        alert("Computer wins! Paper beats rock!");
-        computerScore += 1;
-    }
-    else if (humanChoice === 1 && computerChoice === 3) {
-        alert("Human wins! Rock beats scissors");
-        humanScore += 1;
-    }
-    else if (humanChoice === 2 && computerChoice === 1) {
-        alert("Human wins! Paper beats rock!");
-        humanScore += 1;
-    }
-    else if (humanChoice === 2 && computerChoice === 3) {
-        alert("Computer wins! Scissors beat paper!");
-        computerScore += 1;
-
-    }
-    else if (humanChoice === 3 && computerChoice === 1) {
-        alert("Computer wins! Rock beats scissors!");
-        computerScore += 1;
-
+    else if (computerScore < humanScore) {
+        console.log(`Human wins with score ${humanScore} : ${computerScore}`);
     }
     else {
-        alert("Human wins! Scissors beat paper!")
-        humanScore += 1;
+        console.log(`Tie game! Score was ${humanScore} : ${computerScore}`);
     }
 
-console.log(`Human has ${humanScore} point, compuer has ${computerScore} points`);
+    alert("GAME OVER");
 }
 
-// Define two new variables (cons) and save result of human and computer choices in them
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-// Call playRound function and pass those two variables as arguments
-playRound(humanSelection, computerSelection);
-
-
+// Call play game function to start the game
+playGame();
 
