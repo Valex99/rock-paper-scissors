95538ee2522c59e34df453460675f10ea4a3eb9f
valex <mval.fx@gmail.com>
2024-10-07 15:04:50 +0200

    Add user interface for rock-paper-scissors game, replacing console-only version with browser playability.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// COPY OF LATEST VERSION IN SANDBOX
 let computerScore = 0;
 let humanScore = 0;
 
@@ -5,7 +6,6 @@ let humanScore = 0;
 // Function returns random computer choice for rock/paper/scissors
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-
     if (computerChoice === 1) {
         console.log("Computer choice is rock");
     }
@@ -20,74 +20,97 @@ function getComputerChoice() {
 }
 
 
-// Function checks for valid human input and returns human choice
+// Get human choice with buttons
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper or scissors");
+    let humanChoice = 0;
 
-    // Check for valid input
-    humanChoice = humanChoice.toLowerCase();
-
-    if (humanChoice === "rock") {
-        console.log("Human choice is rock");
+    const rock = document.querySelector('.rock');
+    rock.addEventListener('click', function() {
+        console.log('Human choice is: rock!'); 
         humanChoice = 1;
-    }
-    else if (humanChoice === "paper") {
-        console.log("Human choice is paper");
+        playRound(humanChoice, getComputerChoice());
+    });
+
+    const paper = document.querySelector('.paper');
+    paper.addEventListener('click', function() {
+        console.log('Human choice is: paper!');
         humanChoice = 2;
-    }
-    else if (humanChoice === "scissors") {
-        console.log("Human choice is scissors");
+        playRound(humanChoice, getComputerChoice());
+    });
+
+    const scissors = document.querySelector('.scissors');
+    scissors.addEventListener('click', function() {
+        console.log('Human choice is: scissors!');
         humanChoice = 3;
+        playRound(humanChoice, getComputerChoice());
+
+        // Try to return value at the bottom so you only have to do it once
+    });
+};
+
+// Function plays a single round of a game
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Tie!");
     }
-    else {
-        console.log("Invalid input");
-        getHumanChoice();
-    }
-
-    return humanChoice;
-}
-
-
-// Function that plays entire game (5 rounds of playRound function)
-function playGame() {
-
-    // Function plays a single round of a game
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log("Tie!");
-        }
-        else if (humanChoice === 1) {
-            if (computerChoice === 2) {
-                console.log("Computer wins! Paper beats rock!");
-                computerScore += 1;
-            }
-            else {
-                console.log("Human wins! Rock beats scissors");
-                humanScore += 1;
-            }
-        }
-        else if (humanChoice === 2) {
-            if (computerChoice === 1) {
-                console.log("Human wins! Paper beats rock!");
-                humanScore += 1;
-            }
-            else {
-                console.log("Computer wins! Scissors beat paper!");
-                computerScore += 1; 
-            }
+    else if (humanChoice === 1) {
+        if (computerChoice === 2) {
+            console.log("Computer wins! Paper beats rock!");
+            computerScore += 1;
         }
         else {
-            if (computerChoice === 1) {
-                console.log("Computer wins! Rock beats scissors!");
-                computerScore += 1; 
-            }
-            else {
-                console.log("Human wins! Scissors beat paper!");
-                humanScore += 1; 
-            }
+            console.log("Human wins! Rock beats scissors");
+            humanScore += 1;
         }
     }
-    
+    else if (humanChoice === 2) {
+        if (computerChoice === 1) {
+            console.log("Human wins! Paper beats rock!");
+            humanScore += 1;
+        }
+        else {
+            console.log("Computer wins! Scissors beat paper!");
+            computerScore += 1; 
+        }
+    }
+    else {
+        if (computerChoice === 1) {
+            console.log("Computer wins! Rock beats scissors!");
+            computerScore += 1; 
+        }
+        else {
+            console.log("Human wins! Scissors beat paper!");
+            humanScore += 1; 
+        }
+    }
+console.log(`Human score is: ${humanScore} vs. compuer score: ${computerScore}`)
+}
+
+getHumanChoice();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Removed
+// Function that plays entire game (5 rounds of playRound function)
+
+// function playGame() {
+
+
+
+
+    // Temporarily removed logic that plays exactly 5 rounds
+/*
     // For loop that calls the playRound function 5 times
     for (let i = 0; i < 5; i++) {
         
@@ -111,8 +134,4 @@ function playGame() {
         console.log(`Tie game! Score was ${humanScore} : ${computerScore}`);
     }
     alert("GAME OVER");
-}
-
-// Call play game function to start the game
-playGame();
-
+*/
